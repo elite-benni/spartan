e02ce90313650a68e76773e81b643724edfd9246
elite-benni <benjamin.heiss@elite.tirol>
2025-02-05 21:47:10 +0000

    refactor(dialog): move effects to constructor

--- libs/brain/dialog/src/lib/brn-dialog-overlay.component.ts
+++ libs/brain/dialog/src/lib/brn-dialog-overlay.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, effect, inject, input, ViewEncapsulation } from '@angular/core';
+import { ChangeDetectionStrategy, Component, effect, inject, input, untracked, ViewEncapsulation } from '@angular/core';
 import { provideCustomClassSettableExisting } from '@spartan-ng/brain/core';
 import { BrnDialogComponent } from './brn-dialog.component';
 
@@ -14,13 +14,15 @@ export class BrnDialogOverlayComponent {
 	private readonly _brnDialog = inject(BrnDialogComponent);
 
 	public readonly className = input<string | null | undefined>(undefined, { alias: 'class' });
-	private readonly _classEffect = effect(() => {
-		if (!this._brnDialog) return;
-		const newClass = this.className();
-		this._brnDialog.setOverlayClass(newClass);
-	});
 
 	setClassToCustomElement(newClass: string) {
 		this._brnDialog.setOverlayClass(newClass);
+	}
+	constructor() {
+		effect(() => {
+			if (!this._brnDialog) return;
+			const newClass = this.className();
+			untracked(() => this._brnDialog.setOverlayClass(newClass));
+		});
 	}
 }
--- libs/brain/popover/src/lib/brn-popover-trigger.directive.ts
+++ libs/brain/popover/src/lib/brn-popover-trigger.directive.ts
@@ -16,24 +16,25 @@ import type { BrnPopoverComponent } from './brn-popover.component';
 export class BrnPopoverTriggerDirective extends BrnDialogTriggerDirective {
 	private readonly _host = inject(ElementRef, { host: true });
 
-	constructor() {
-		super();
-		if (!this._brnDialog) return;
-		this._brnDialog.attachToState().set(this._host.nativeElement);
-		this._brnDialog.closeOnOutsidePointerEventsState().set(true);
-	}
-
 	public readonly brnPopoverTriggerFor = input<BrnPopoverComponent | undefined>(undefined, {
 		alias: 'brnPopoverTriggerFor',
 	});
-	private readonly _brnPopoverTriggerForEffect = effect(
-		() => {
-			const brnDialog = this.brnPopoverTriggerFor();
-			if (!brnDialog) return;
-			brnDialog.attachToState().set(this._host.nativeElement);
-			brnDialog.closeOnOutsidePointerEventsState().set(true);
-			this.brnDialogTriggerForState().set(brnDialog);
-		},
-		{ allowSignalWrites: true },
-	);
+
+	constructor() {
+		super();
+		if (!this._brnDialog) return;
+		this._brnDialog.mutableAttachTo().set(this._host.nativeElement);
+		this._brnDialog.mutableCloseOnOutsidePointerEvents().set(true);
+
+		effect(
+			() => {
+				const brnDialog = this.brnPopoverTriggerFor();
+				if (!brnDialog) return;
+				brnDialog.mutableAttachTo().set(this._host.nativeElement);
+				brnDialog.mutableCloseOnOutsidePointerEvents().set(true);
+				this.brnDialogTriggerForState().set(brnDialog);
+			},
+			{ allowSignalWrites: true },
+		);
+	}
 }
